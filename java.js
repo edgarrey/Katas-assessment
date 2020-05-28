6524b1cf2e6d7d3dc64a6a807e3e56be10465a5a
Edgar <some1just@gmail.com>
2020-05-28 15:01:46 -0600

    fixed to not use inc

--- java.js
+++ java.js
@@ -7,35 +7,36 @@ function add (x, y){
 console.log(add(2,4))
 
 // kata 02
-function multiply(num, times){
+function multiply(a, b){
     
-    var n = num;
-    for(var i = 1; i < times; i += 1)
-       num += n; 
-    return num;
+    let ans = a 
+    for (let i= 0; i < b - 1; i++){
+    ans = add (a , ans)
  } 
+ return ans
+} 
+
  console.log(multiply(6, 4))
 
 //  kata 03
 
-function Exponents (num, exponent) {
-        var result = 1;
-        for (i = 0; i < exponent; i += 1) {
-        result *= num;
+function Exponents (x, n) {
+        let ans = x
+        for (let i = 0; i < n - 1; i += 1) {
+        ans = multiply(ans, x)
         }
-        return result;
+        return ans
     } 
 console.log(Exponents(2, 8))
 
 // kata 04
-function factorial(number){
-    var result = number;
-    if (number === 0 || number === 1) 
+function factorial(num){
+    
+    if (num === 0 || num === 1) 
       return 1; 
-    while (number > 1) { 
-      number -= 1;
-      result *= number;
-    }
-    return result;
+   for (var i = num - 1; i >=1 ; i--){
+    num = multiply(i, num);
   }
+return num
+}
 console.log(factorial(4))
